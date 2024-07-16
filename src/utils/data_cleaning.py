e4a9ba94d6914e58c0a800d0565f684b2492ec8d
walter2310 <walter.partera@back9.com.ve>
2024-07-16 11:25:48 -0400

    feat: combine percentiles and zscore

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -19,11 +19,21 @@ def apply_one_hot_encoding(df, categorical_column):
     df = df.join(dummies).drop([categorical_column], axis=1)
     return df
 
-def remove_outliers_zscore(df, threshold=3):
+def remove_outliers_zscore(df, z_threshold=2.5, percentile_threshold=0.01):
+    # Calcular Z-scores
     z_scores = stats.zscore(df.select_dtypes(include=['float64', 'int64']))
     abs_z_scores = np.abs(z_scores)
 
-    filtered_entries = (abs_z_scores < threshold).all(axis=1)
-    df_filtered = df[filtered_entries]
+    # Filtrado por Z-score
+    z_filtered = (abs_z_scores < z_threshold).all(axis=1)
+
+    # Filtrado por percentiles
+    lower_bound = df.quantile(percentile_threshold)
+    upper_bound = df.quantile(1 - percentile_threshold)
+    percentile_filtered = (df >= lower_bound) & (df <= upper_bound)
+
+    # Combina ambos filtros
+    combined_filter = z_filtered & percentile_filtered.all(axis=1)
+    df_filtered = df[combined_filter]
 
     return df_filtered
